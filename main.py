e881dd410c8280abfdbe70d698063efca45951ed
Jacob Kanev <jkanev@zoho.com>
2024-04-25 22:24:28 +0200

    Minor change for Windows
    Fix to deal with the missing I/O pipes when in the Windows pyinstaller
    environment.

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import sys
 # main starting file for use with or with pyinstaller for creating executable binaries.
 # For normal running please execute the software as a module (python3 -m treetime);
 # this will started the treeview/__main__.py file.
-import faulthandler
+#import faulthandler
 from treetime.treetime import TreeTime
 
-faulthandler.enable()
+#faulthandler.enable()
 if len(sys.argv)==2:
     TreeTime(filename=sys.argv[1])
 else:
